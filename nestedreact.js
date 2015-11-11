bdf91796c82edef110a2fda7a63dec079df1d100
Vlad Balin <gaperton@gmail.com>
2015-11-11 18:34:07 -0500

    prevent render if subview options are identical

--- nestedreact.js
+++ nestedreact.js
@@ -318,12 +318,45 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var React = __webpack_require__( 1 );
 	
+	function shallowEqual(objA, objB) {
+	    if (objA === objB) {
+	        return true;
+	    }
+	
+	    if (typeof objA !== 'object' || objA === null ||
+	        typeof objB !== 'object' || objB === null) {
+	        return false;
+	    }
+	
+	    var keysA = Object.keys(objA);
+	    var keysB = Object.keys(objB);
+	
+	    if (keysA.length !== keysB.length) {
+	        return false;
+	    }
+	
+	    // Test for A's keys different from B.
+	    var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+	    for (var i = 0; i < keysA.length; i++) {
+	        if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+	            return false;
+	        }
+	    }
+	
+	    return true;
+	}
+	
 	module.exports = React.createClass({
 	    displayName : 'BackboneView',
 	
 	    propTypes : {
 	        View : React.PropTypes.func.isRequired,
 	        options : React.PropTypes.object
+	    },
+	
+	    shouldComponentUpdate : function( next ){
+	        var props = this.props;
+	        return next.View !== props.View || !shallowEqual( next.options, props.options );
 	    },
 	
 	    render : function(){
--- src/view-element.js
+++ src/view-element.js
@@ -1,11 +1,44 @@
 var React = require( 'react' );
 
+function shallowEqual(objA, objB) {
+    if (objA === objB) {
+        return true;
+    }
+
+    if (typeof objA !== 'object' || objA === null ||
+        typeof objB !== 'object' || objB === null) {
+        return false;
+    }
+
+    var keysA = Object.keys(objA);
+    var keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+    for (var i = 0; i < keysA.length; i++) {
+        if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 module.exports = React.createClass({
     displayName : 'BackboneView',
 
     propTypes : {
         View : React.PropTypes.func.isRequired,
         options : React.PropTypes.object
+    },
+
+    shouldComponentUpdate : function( next ){
+        var props = this.props;
+        return next.View !== props.View || !shallowEqual( next.options, props.options );
     },
 
     render : function(){
