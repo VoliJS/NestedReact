a5d1d0aab0f64816dc0ae7a701131730ecf221d8
Vlad Balin <gaperton@gmail.com>
2016-01-06 18:20:00 -0500

    added type information extraction

--- src/decorator.js
+++ src/decorator.js
@@ -1,13 +1,14 @@
-
+var React  = require( 'react' ),
+    Nested = require( 'nestedtypes' );
 
 Object.extend.attach( React.Component );
 Object.assign( React.Component, Events );
 
-React.Component.extend({
-    Model : null,
+React.Component.extend( {
+    Model    : null,
     autobind : null,
 
-    constructor: function( props ){
+    constructor : function( props ){
         React.Component.call( this, props );
 
         if( this.Model ){
@@ -18,7 +19,7 @@ React.Component.extend({
         if( autobind ){
             for( var i = 0; i < autobind.length; i++ ){
                 var name = autobind[ i ],
-                    fun = this[ name ];
+                    fun  = this[ name ];
 
                 if( fun ){
                     this[ name ] = fun.bind( this );
@@ -26,15 +27,50 @@ React.Component.extend({
             }
         }
     }
-});
+} );
+
+function convertType( Type ){
+    switch( Type ){
+        case String :
+            return PropTypes.string;
+        case Number :
+            return PropTypes.number;
+        case Integer :
+            return PropTypes.number;
+        case Boolean :
+            return PropTypes.bool;
+        case Function :
+            return PropTypes.func;
+        case Array :
+            return PropTypes.array;
+        case Object :
+            return PropTypes.object;
+        default :
+            return Type.prototype ? PropTypes.instanceOf( Type ) : Type;
+    }
+}
+
+function parseProps( spec ){
+    var PropsModel   = Model.defaults( spec ),
+        propTypes    = {},
+        defaultProps = {};
+
+    PropsModel.prototype.forEachAttr( PropsModel.prototype.__attributes, function( spec, name ){
+        if( spec.value ){
+            defaultProps[ name ] = spec.value;
+        }
+
+        propTypes[ name ] = convertType( spec.type );
+    } );
+}
 
 React.define = function( options ){
     return function( Class ){
         var spec = {};
 
         if( options.props ){
-            var propSpec = parseProps( options.props );
-            Class.propTypes = propSpec.propTypes;
+            var propSpec       = parseProps( options.props );
+            Class.propTypes    = propSpec.propTypes;
             Class.defaultProps = propSpec.defaultProps;
         }
 
