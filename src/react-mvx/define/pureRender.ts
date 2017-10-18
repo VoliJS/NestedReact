ce0d127ba65a9bd05172402b723f51a52f9e6ee1
Vlad Balin <gaperton@gmail.com>
2017-10-18 15:59:56 -0400

    pure render bugs

--- src/react-mvx/define/pureRender.ts
+++ src/react-mvx/define/pureRender.ts
@@ -11,7 +11,7 @@ export function createChangeTokensConstructor( props ) {
     
     PropsChangeTokens.prototype._hasChanges = new Function( 'p', 's', `
         var v;
-        return ( s && s._changeToken !== this._s ) ${ propNames.map( name => ` ||
+        return ( ( s && s._changeToken ) !== this._s ) ${ propNames.map( name => ` ||
             this.${ name } !== ( ( ( v = p.${ name }) && v._changeToken ) || v )
         `).join( '' )};
     `);    
